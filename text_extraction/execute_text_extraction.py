478f9d96577cccc457d31c5074d006bec7153ee1
Fernando <fernando.rpalvarez@gmail.com>
2023-08-11 22:42:09 +0200

    Text extraction completed and saved into txt files

--- text_extraction/execute_text_extraction.py
+++ text_extraction/execute_text_extraction.py
@@ -1,10 +1,15 @@
 import pdf_text_extractor as pdf_te
 import pdf_tool_manager
+import json
 
 
 if __name__ == '__main__':
-    pdfs_path = "C:/Users/ferna/Universidad Politécnica de Madrid/Linea Accesibilidad Cognitiva (Proyecto)-Corpus Lectura Fácil (2023) - Documentos/data/pdfs_from_web/plena_inclusion_extremadura"
-    output_path_for_extracted_text = "C:/Users/ferna/Universidad Politécnica de Madrid/Linea Accesibilidad Cognitiva (Proyecto)-Corpus Lectura Fácil (2023) - Documentos/data/extracted_text_from_pdfs"
+    config = json.load(open("../config/pdf_to_text_extraction_config.json", "r", encoding="utf-8"))
+    pdfs_path = config["pdfs_path"]
+    output_path_for_extracted_text = config["output_path_for_extracted_text"]
+    rename_files_flag = config["rename_files_flag"]
 
-    pdf_tool_manager.rename_files_in_path(pdfs_path)
-    # pdf_te.extract_text_from_pdfs_in_path(pdfs_path, output_path_for_extracted_text)
+    if rename_files_flag:
+        pdf_tool_manager.rename_files_in_path(pdfs_path)
+
+    pdf_te.extract_text_from_pdfs_in_subdirs_path(pdfs_path, output_path_for_extracted_text)
--- text_extraction/pdf_text_extractor.py
+++ text_extraction/pdf_text_extractor.py
@@ -1,6 +1,5 @@
 import os
-
-import pandas as pd
+import shutil
 from PyPDF2 import PdfReader
 
 
@@ -21,15 +20,24 @@ def extract_text_from_pdf(pdf_path):
 
 def extract_text_from_pdfs_in_path(pdfs_path, output_path_for_extracted_text):
     for pdf_path in os.listdir(pdfs_path):
-        pdf_text = extract_text_from_pdf(os.path.join(pdfs_path, pdf_path))
+        # Extract text from pdfs into txt files
+        if pdf_path.endswith(".pdf"):
+            try:
+                pdf_text = extract_text_from_pdf(os.path.join(pdfs_path, pdf_path))
+                output_text_path = os.path.join(output_path_for_extracted_text, pdf_path.split(".pdf")[0]) + ".txt"
+                with open(output_text_path, 'w', encoding='utf-8') as f:
+                    f.write(pdf_text)
+            except:
+                pass
 
-        output_text_path = os.path.join(output_path_for_extracted_text, pdf_path.split(".pdf")[0]) + ".txt"
-        with open(output_text_path, 'w') as f:
-            f.write(pdf_text)
+        # Copy text files into new path
+        elif pdf_path.endswith(".txt"):
+            output_text_path = os.path.join(output_path_for_extracted_text, pdf_path)
+            shutil.copy2(os.path.join(pdfs_path, pdf_path), output_text_path)
 
 
 def extract_text_from_pdfs_in_subdirs_path(subdirs_path, output_path):
-    for root, subdir, files in not os.walk(subdirs_path):
+    for root, subdir, files in os.walk(subdirs_path):
         if files:
             output_path_for_extracted_text = os.path.join(output_path, root.split("/")[-1])
             if not os.path.isdir(output_path_for_extracted_text):
