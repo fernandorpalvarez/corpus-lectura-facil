31780f6da16e778b16f765aaf0e9e5b8c147a19e
Fernando <fernando.rpalvarez@gmail.com>
2023-09-18 19:07:25 +0200

    Ad-hoc operation definition

--- corpus_creation/lectura_facil_pipeline.py
+++ corpus_creation/lectura_facil_pipeline.py
@@ -2,6 +2,15 @@ import os
 import re
 import pandas as pd
 from bs4 import BeautifulSoup
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import stanza
+from tqdm import tqdm
+
+# nltk.download('stopwords')
+# nltk.download('punkt')
+# stanza.download("es")
 
 
 # Data gathering (tokenization)
@@ -49,6 +58,8 @@ def apply_pipeline(df):
     df = apply_special_char_removal(df)
     df = apply_minor_casing(df)
     df = apply_null_values_management(df)
+    df = apply_stop_words_removal(df)
+    df = apply_lemmatizing(df)
     df.drop_duplicates(inplace=True)
     df.reset_index(drop=True, inplace=True)
 
@@ -75,8 +86,32 @@ def apply_minor_casing(df):
 
 
 # Stopwords removal
+def apply_stop_words_removal(df):
+    stop_words_sp = set(stopwords.words('spanish'))
 
-# Lemmatizing or stemming
+    def filter_stop_words(txt, stop_words):
+        word_tokens = word_tokenize(txt)
+        # converts the words in word_tokens to check whether
+        # they are present in stop_words or not
+        filtered_sentence = [w for w in word_tokens if not w in stop_words]
+        return ' '.join(filtered_sentence)
+
+    df['text'] = df['text'].apply(lambda x: filter_stop_words(str(x), stop_words_sp))
+
+    return df
+
+
+# Lemmatizing
+def apply_lemmatizing(df):
+    def lemmatization_for_row(row, nlp):
+        lemmas = [word.lemma for sent in nlp(str(row)).sentences for word in sent.words]
+        return " ".join(lemmas)
+
+    tqdm.pandas()
+    stanza_nlp = stanza.Pipeline(lang='es', processors='tokenize,mwt,pos,lemma')
+    df['text'] = df['text'].progress_apply(lambda x: lemmatization_for_row(x, stanza_nlp))
+    return df
+
 
 # Numeric characters removal
 def apply_numeric_char_removal(df):
@@ -86,37 +121,32 @@ def apply_numeric_char_removal(df):
 
 # HTML tags removal
 def apply_html_tag_removal(df):
-    '''
-    df['text'] = df['text'].str.replace('<.*?>', '')
-    # Use re.sub() to replace matched URLs with an empty string
-    patterns_to_apply = [r'\bhttp\.[^\s]+\b', r'\bwww\.[^\s]+\b', r'[^\s]+.http[^\s]+', r'[^\s]+.www[^\s]+',
-                         r'[^\s]+.org[^\s]+', r'[^\s]+.org', r'[^\s]+.es[^\s]+', r'[^\s]+.es', r'[^\s]+.com[^\s]+',
-                         r'[^\s]+.com', r'.com', r'www.']
-    for pattern in patterns_to_apply:
-        df['text'] = df['text'].apply(lambda x: re.sub(pattern, '', str(x)))
-    '''
+    def remove_html_tags(text):
+        # Eliminar etiquetas HTML
+        soup = BeautifulSoup(text, 'html.parser')
+        cleaned_text = soup.get_text()
+
+        # Eliminar enlaces
+        cleaned_text = re.sub(r'http\S+|www\S+|https\S+', '', cleaned_text, flags=re.MULTILINE)
+
+        return cleaned_text
+
     df['text'] = df['text'].apply(lambda x: remove_html_tags(str(x)))
     return df
 
 
-def remove_html_tags(text):
-    # Eliminar etiquetas HTML
-    soup = BeautifulSoup(text, 'html.parser')
-    cleaned_text = soup.get_text()
-
-    # Eliminar enlaces
-    cleaned_text = re.sub(r'http\S+|www\S+|https\S+', '', cleaned_text, flags=re.MULTILINE)
-
-    return cleaned_text
-
-
-# 8. Empty or null values management
+# Empty or null values management
 def apply_null_values_management(df):
     df = df[df['text'] != ""]
     df['text'] = df['text'].dropna()
     return df
 
 
-# 9. Saving results
+# Pending
+def apply_ad_hoc_operations(df):
+    pass
+
+
+# Saving results
 def save_dataframe_in_path(df, path):
     df.to_csv(os.path.join(path, "lectura_facil.csv"), sep="|", index=False, encoding="utf-8")
