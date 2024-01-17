967dc1a15abdc926ff6df27f20c37a41802f686d
fernandorpalvarez <fernando.rpalvarez@gmail.com>
2024-01-18 00:30:26 +0100

    Implemented tag function and corpus combiner

--- src/corpus_creation/corpus_combiner.py
+++ src/corpus_creation/corpus_combiner.py
@@ -1,11 +1,15 @@
+import pandas as pd
+
 """
     Script for tagging and combining natural language corpus and 'lectura facil' corpus
 """
 
 
-def tag_corpus():
-    pass
+def tag_data(df: pd.DataFrame, tag: object):
+    df["class"] = tag
+
+    return df.copy(deep=True)
 
 
-def combine_corpus():
-    pass
+def combine_corpus(df_list: list):
+    return pd.concat(df_list)
--- src/corpus_creation/execute_corpus_creation.py
+++ src/corpus_creation/execute_corpus_creation.py
@@ -1,5 +1,6 @@
 from corpus_combiner import *
 import json
+from src.tools import dataframe_tools
 
 
 if __name__ == '__main__':
@@ -8,5 +9,19 @@ if __name__ == '__main__':
             clean -> transform
     '''
     config = json.load(open("../../config/corpus_creation_config.json", "r", encoding="utf-8"))
-    extracted_text_path = config["extracted_text_path"]
-    corpus_lectura_facil_path = config["corpus_lectura_facil_path"]
+    clean_text_path = config["clean_text_path"]
+    transform_text_path = config["transform_text_path"]
+    clean_text_name = config["clean_text_name"]
+    transform_text_name = config["transform_text_name"]
+
+    # Get the text
+    clean_text_df = dataframe_tools.read_dataframe(clean_text_path, clean_text_name)
+
+    # Tag the dataframe
+    clean_text_df = tag_data(clean_text_df, 1)
+
+    # Combine with natural language
+    final_corpus_df = combine_corpus([clean_text_df])  # TODO: Add natural language
+
+    # Save the outputs
+    dataframe_tools.write_dataframe(final_corpus_df, transform_text_path, transform_text_name)
